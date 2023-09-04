11e54dbfe3e12c5bf797d45728657c7829e164e8
nagasai <nagasaij@iisc.ac.in>
2023-09-04 18:43:29 +0530

    Added secant_method

--- root_finding.py
+++ root_finding.py
@@ -1,3 +1,5 @@
+import math as m
+
 def bisection_method(function, left_end, right_end, tolerance, max_iterations):
     iteration = 0
     while (iteration < max_iterations):
@@ -41,3 +43,18 @@ def newton_method(function, function_derivative, initial_guess, tolerance, max_i
         
         initial_guess = new_guess
     return initial_guess   
+
+
+def secant_method(function, guess1, guess2, tolerance, max_iterations):
+    iteration = 0
+    while(iteration < max_iterations):
+        iteration += 1
+        function_derivative = (function(guess2)-function(guess1))/(guess2-guess1)
+        new_guess = guess2 - (function(guess2)/function_derivative)
+        guess1 = guess2
+        guess2 = new_guess
+        if(abs(guess2-guess1) < tolerance):
+            return guess2
+
+    return guess2
+
