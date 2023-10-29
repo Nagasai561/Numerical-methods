5fa4cda78be02677fcf9cdb888a2356e37dd161a
Nagasai <nagasaij@iisc.ac.in>
2023-10-29 10:14:43 +0530

    updated integration

--- integration.py
+++ integration.py
@@ -8,7 +8,7 @@ def trapezoidal_rule(function, a, b):
     return (function(a) + function(b))*((b-a)/2)
 
 def simpson_rule(function, a, b):
-    return (function(a) + 4*function((a+b)/2)+ function(b))*(b-a)/6
+    return (function(a) + 4*function((a+b)/2)+ function(b))*((b-a)/6)
 
 def composite_rules(function_rule, function, a, b, num_intervals):
     step_size = (b-a)/num_intervals
@@ -18,7 +18,7 @@ def composite_rules(function_rule, function, a, b, num_intervals):
     return result
 
 def gaussian_rule(function, n):
-    roots = [ [-0.5773502692, 0.5773502692], [-0.7745966692, 0, 0.7745966692], [-0.8611363116, -0.3399810436, 0, 0.3399810436, 0.8611363116]]
+    roots = [[-0.5773502692, 0.5773502692], [-0.7745966692, 0, 0.7745966692], [-0.8611363116, -0.3399810436, 0, 0.3399810436, 0.8611363116]]
     coefficients = [[1, 1], [0.55555556,0.88888889, 0.55555556], [0.3478548451, 0.6521451549, 0.6521451549, 0.3478548451]]
     root = roots[n-2]
     coefficient = coefficients[n-2]
